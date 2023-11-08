b51c2db6b020ad01ef04a9b8df02ba6c4bf492e6
Agustina <agustinacastro37@gmail.com>
2023-11-07 23:35:01 -0300

    direccion al carrito

--- src/main/java/ecommerce/donatto/controller/HomeController.java
+++ src/main/java/ecommerce/donatto/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ecommerce.donatto.model.Product;
@@ -41,5 +42,10 @@ public class HomeController {
         model.addAttribute("product", product);
 
         return "user/producthome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart() {
+        return "user/cart";
     }
 }
