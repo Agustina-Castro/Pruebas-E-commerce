5c76891de58d3b6e38e1f7fd56769e9144fb5ac2
Agustina <agustinacastro37@gmail.com>
2023-11-07 22:49:37 -0300

    mostrar la vista del usuario

--- src/main/java/ecommerce/donatto/controller/HomeController.java
+++ src/main/java/ecommerce/donatto/controller/HomeController.java
@@ -1,9 +1,12 @@
 package ecommerce.donatto.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ecommerce.donatto.service.ProductService;
@@ -11,6 +14,8 @@ import ecommerce.donatto.service.ProductService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log= LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductService productService;
@@ -21,5 +26,11 @@ public class HomeController {
         model.addAttribute("products", productService.findAll());
 
         return "user/home";
+    }
+
+    @GetMapping("producthome/{id}")
+    public String productHome(@PathVariable Integer id) {
+        log.info("id enviado como parametro {}",id);
+        return "user/producthome";
     }
 }
